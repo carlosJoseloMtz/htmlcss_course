26d8009d180204110d22e57d9042af338c14007c
RafaelArrieta <rafael2023@gmail.com>
2015-06-30 18:02:14 -0500

    prueba blog

--- rafael(ccarlos)/Pokemon/src/InterfacePoke.java
+++ rafael(ccarlos)/Pokemon/src/InterfacePoke.java
@@ -3,6 +3,5 @@ public interface InterfacePoke {
 	void curarPokemon (Pokemon pokemon);
 	void evolucionarPokemon(Pokemon pokemon);
 	void aumentosPokemon(Pokemon pokemon);
-	void ataque(Pokemon pokemon);
-	
+	void ataque(Pokemon pokemon);	
 }
